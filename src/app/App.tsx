b66a0923f9bf32e95f74d3680a9cb1c01a2c224a
RomanCox <rom.cox@gmail.com>
2022-11-07 19:39:11 +0300

    testing commit for deploy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -36,29 +36,32 @@ export const App = (props: AppPropsType) => {
     }
 
     return (
-        <AppContainerStyled>
-            <ErrorSnackBar/>
-            <AppBar position={'static'}>
-                <Toolbar>
-                    <IconButton edge={'start'} color={'inherit'} aria-label={'menu'}>
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant={'h6'}>
-                        News
-                    </Typography>
-                    {isLoggedIn && <Button color={'inherit'} onClick={logoutHandler}>Log out</Button>}
-                </Toolbar>
-                {status === 'loading' && <LinearProgress/>}
-            </AppBar>
-            <Container fixed>
-                <Routes>
-                    <Route path='/' element={<TodoListsList demo={false} />}/>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
-                    <Route path='*' element={<Navigate to='/404'/>}/>
-                </Routes>
-            </Container>
-        </AppContainerStyled>
+        // <AppContainerStyled>
+        //     <ErrorSnackBar/>
+        //     <AppBar position={'static'}>
+        //         <Toolbar>
+        //             <IconButton edge={'start'} color={'inherit'} aria-label={'menu'}>
+        //                 <Menu/>
+        //             </IconButton>
+        //             <Typography variant={'h6'}>
+        //                 News
+        //             </Typography>
+        //             {isLoggedIn && <Button color={'inherit'} onClick={logoutHandler}>Log out</Button>}
+        //         </Toolbar>
+        //         {status === 'loading' && <LinearProgress/>}
+        //     </AppBar>
+        //     <Container fixed>
+        //         <Routes>
+        //             <Route path='/' element={<TodoListsList demo={false} />}/>
+        //             <Route path='/login' element={<Login/>}/>
+        //             <Route path='/404' element={<h1>404: PAGE NOT FOUND</h1>}/>
+        //             <Route path='*' element={<Navigate to='/404'/>}/>
+        //         </Routes>
+        //     </Container>
+        // </AppContainerStyled>
+        <div>
+            привет
+        </div>
     );
 };
 
